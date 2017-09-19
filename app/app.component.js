114f13c9838db395afb651622e83978cbfe96d45
Guest User <Guest@epicodus-25.local>
2017-09-19 16:22:38 -0700

    initial

--- app/app.component.js
+++ app/app.component.js
@@ -14,17 +14,54 @@ var AppComponent = (function () {
         this.month = this.currentTime.getMonth() + 1;
         this.day = this.currentTime.getDate();
         this.year = this.currentTime.getFullYear();
-        this.firstTask = {
-            description: "Finish weekend Angular homework for Epicodus course"
-        };
+        this.tasks = [
+            new Task('Finish weekend Angular homework for Epicodus course', 3),
+            new Task('Begin brainstorming possible JavaScript group projects', 2),
+            new Task('Add README file to last few Angular repos on GitHub', 1)
+        ];
+        this.selectedTask = null;
     }
+    AppComponent.prototype.editTask = function (clickedTask) {
+        this.selectedTask = clickedTask;
+    };
+    AppComponent.prototype.isDone = function (clickedTask) {
+        if (clickedTask.done === true) {
+            alert("this task is completed/done");
+        }
+        else {
+            alert("this task is not done/completed");
+        }
+    };
+    AppComponent.prototype.priorityColor = function (currentTask) {
+        if (currentTask.priority === 3) {
+            return "bg-danger";
+        }
+        else if (currentTask.priority === 2) {
+            return "bg-warning";
+        }
+        else {
+            return "bg-info";
+        }
+    };
+    AppComponent.prototype.finishedEditing = function () {
+        this.selectedTask = null;
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'app-root',
-        template: "\n    <div class=\"container\">\n      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>\n      <h3>{{currentFocus}}</h3>\n      <ul>\n        <li>{{firstTask.description}}</li>\n      </ul>\n    </div>\n  "
+        template: "\n    <div class=\"container\">\n      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>\n      <h3>{{currentFocus}}</h3>\n      <ul>\n       <li [class]=\"priorityColor(currentTask)\" (click) = \"isDone(currentTask)\" *ngFor=\"let currentTask of tasks\"> {{currentTask.description}}  <button (click)=\"editTask(currentTask)\">Edit!</button></li>\n     </ul>\n     <div>\n      <div *ngIf=\"selectedTask\">\n        <h3>{{selectedTask.description}}</h3>\n        <p>Task Complete? {{selectedTask.done}}</p>\n        <h3>Edit Task</h3>\n        <label>Enter Task Description:</label>\n        <input [(ngModel)]=\"selectedTask.description\">\n        <label>Enter Task Priority (1-3):</label>\n        <br>\n        <input type=\"radio\" [(ngModel)]=\"selectedTask.priority\" [value]=\"1\">1 (Low Priority)<br>\n        <input type=\"radio\" [(ngModel)]=\"selectedTask.priority\" [value]=\"2\">2 (Medium Priority)<br>\n        <input type=\"radio\" [(ngModel)]=\"selectedTask.priority\" [value]=\"3\">3 (High Priority)\n        <button (click)=\"finishedEditing()\">Done</button>\n      </div>\n     </div>\n    </div>\n  "
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
+var Task = (function () {
+    function Task(description, priority) {
+        this.description = description;
+        this.priority = priority;
+        this.done = false;
+    }
+    return Task;
+}());
+exports.Task = Task;
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -7,8 +7,23 @@ import { Component } from '@angular/core';
       <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
       <h3>{{currentFocus}}</h3>
       <ul>
-       <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
+       <li [class]="priorityColor(currentTask)" (click) = "isDone(currentTask)" *ngFor="let currentTask of tasks"> {{currentTask.description}}  <button (click)="editTask(currentTask)">Edit!</button></li>
      </ul>
+     <div>
+      <div *ngIf="selectedTask">
+        <h3>{{selectedTask.description}}</h3>
+        <p>Task Complete? {{selectedTask.done}}</p>
+        <h3>Edit Task</h3>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+        <label>Enter Task Priority (1-3):</label>
+        <br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+        <button (click)="finishedEditing()">Done</button>
+      </div>
+     </div>
     </div>
   `
 })
@@ -20,13 +35,43 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+    new Task('Finish weekend Angular homework for Epicodus course',3),
+    new Task('Begin brainstorming possible JavaScript group projects',2),
+    new Task('Add README file to last few Angular repos on GitHub',1)
   ];
+  selectedTask = null;
+  editTask(clickedTask: Task)
+  {
+    this.selectedTask = clickedTask;
+  }
+  isDone(clickedTask: Task)
+  {
+    if(clickedTask.done === true)
+    {
+      alert("this task is completed/done");
+    }
+    else
+    {
+      alert("this task is not done/completed");
+    }
+  }
+  priorityColor(currentTask: Task)
+  {
+    if(currentTask.priority === 3) {
+      return "bg-danger";
+    } else if(currentTask.priority === 2) {
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+
 }
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string) { }
+  constructor(public description: string, public priority: number) { }
 }
